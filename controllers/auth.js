a41269bc75415a70bfea26d3432f737c65bc23f0
aayush-dhakal <aayushdhakal77@gmail.com>
2023-12-10 18:50:13 +0545

    added domain in logout while clearing cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -236,6 +236,7 @@ exports.logout = asyncHandler(async (req, res, next) => {
     secure: process.env.NODE_ENV === "production",
     // sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict",
     sameSite: "None",
+    domain: "https://ai-research-frontend.vercel.app",
   };
 
   // const token = "";
